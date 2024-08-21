5edc4ec0693f84648d2909714c634a1ff10325b6
june276 <jungjune276@gmail.com>
2024-08-21 19:35:54 +0900

    공지사항 위젯

---
+++ lib/screen/notice.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+void main() async{
+  runApp(const Notice());
+  WidgetsFlutterBinding.ensureInitialized();
+}
+
+class Notice extends StatelessWidget {
+  const Notice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar( title: Center(child: Text('공지사항')), backgroundColor: Color(0xFFF0F1F0),),
+        body: NoticeBodyWidget(),
+      ),
+    );
+  }
+}
+
+class NoticeBodyWidget extends StatelessWidget {
+  const NoticeBodyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      width: double.infinity,
+      color: Color(0xFFF0F1F0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          NoticeBox(title: '테스트', content: '테스트 내용입니다. 더 상세한 내용은 안녕하세요 입니다. 이게 무슨 말인지는 모르겠고 그냥 이것 저것 나열하는 중입니다. 이 정도 길이면 충분하지 않을까요'),
+          NoticeBox(title: 'main 공지', content: '색깔이 달라요. 좀 더 강조되는 효과가 있겠죠. 또한 하나 정도라도 있다면 미적으로 좋아 보일 수 있습니다. 왜냐하면 Main Theme Color를 사용했기 때문인데요. 이로 인해 사용자는 더 통일된 디자인을 경험하게 됩니다.', theme: Color(0xFF537052),),
+        ],
+      ),
+    );
+  }
+}
+
+class NoticeBox extends StatefulWidget {
+  final String content;
+  final String title;
+  final Color theme;
+
+  const NoticeBox({
+    super.key,
+    required this.title,
+    required this.content,
+    this.theme = Colors.white,
+  });
+
+  @override
+  State<NoticeBox> createState() => _NoticeBoxState();
+}
+
+class _NoticeBoxState extends State<NoticeBox> {
+  bool _isOpen = false;
+  void _toggleOpen(){
+    setState(() {
+      _isOpen = !_isOpen;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 351,
+      padding: EdgeInsets.all(20),
+      margin: EdgeInsets.fromLTRB(0, 15, 0, 15),
+      decoration: ShapeDecoration(
+        color: widget.theme,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        shadows:  const [
+          BoxShadow(
+            color: Color(0x3F000000),
+            blurRadius: 4,
+            offset: Offset(0,4),
+            spreadRadius: 0,
+          )
+        ]
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(7, 0, 0, 0),
+                child: Text(widget.title,
+                  style: TextStyle(
+                    color: (widget.theme == Colors.white) ? Colors.black : Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              Container(
+                width: 30,
+                height: 30,
+                child: IconButton(
+                  icon: Transform.rotate(
+                    angle:  _isOpen ? -0.5 * 3.14159 : 0.0,
+                    child: const Icon(Icons.arrow_back_ios_new, size: 15,)
+                  ),
+                  color: (widget.theme == Colors.white) ? Colors.black : Colors.white,
+                  onPressed: _toggleOpen
+                )
+              )
+            ],
+          ),
+          Divider(
+            color: (widget.theme == Colors.white) ? Color(0xFFD4D4D4) : Color(0xFF8BA38A), // Color of the divider
+            thickness: 2,       // Thickness of the line
+            height: 20,         // Space around the divider
+          ),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.fromLTRB(7, 0, 7, 0),
+            child: Text(
+              _isOpen ? widget.content : widget.content.substring(0, 10) + '...' ,
+              style: TextStyle(
+                color: (widget.theme == Colors.white) ? Colors.black : Colors.white,
+              ),
+            )
+          ),
+        ],
+      )
+    );
+  }
+}
