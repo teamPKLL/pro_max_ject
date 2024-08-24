f932ce6c9dea837196edd0ac78815ec56d9f10de
june276 <jungjune276@gmail.com>
2024-08-25 01:49:43 +0900

    reminder 디자인 더 깔끔하게..

--- lib/screen/reminder.dart
+++ lib/screen/reminder.dart
@@ -61,13 +61,18 @@ class _ReminderState extends State<Reminder> with AutomaticKeepAliveClientMixin<
         return Scaffold(
           backgroundColor: Color(0xFFF0F1F0),
           appBar: AppBar(
-            title: Text('알림'),
+            title:  Text('알림',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'BM_HANNA_TTF',
+              )
+            ),
             centerTitle: true,
-            backgroundColor: Colors.grey[200],
+            backgroundColor: const Color(0xEF537052),
             elevation: 0,
             actions: [
               IconButton(
-                icon: Icon(Icons.refresh),
+                icon: Icon(Icons.refresh, color: Colors.white,),
                 onPressed: () {
                   _loadData(refresh: true); // 새로고침 시 데이터 로드
                 },
@@ -117,61 +122,103 @@ class _ReminderState extends State<Reminder> with AutomaticKeepAliveClientMixin<
     required String registrationDate,
   }) {
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 8, horizontal: screenWidth * 0.05),
-      height: screenHeight * 0.1,
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-        shadows: [
+      width: double.infinity,
+      margin: EdgeInsets.fromLTRB(30, 15, 30, 15),
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        color: Colors.white, // 배경색
+        boxShadow: [
           BoxShadow(
             color: Color(0x30000000),
             blurRadius: 4,
             offset: Offset(0, 5),
             spreadRadius: 0,
           ),
+        ]
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: double.infinity,
+            child: Text(text,
+              style: const TextStyle(
+                fontSize: 15,
+                fontFamily: 'Lexend Deca',
+              )
+            ),
+          ),
+          const SizedBox(height: 6,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children:[
+              Text(_formatDate(registrationDate).toString().substring(0,19),
+                style: const TextStyle(
+                  color: Colors.black38,
+                  fontSize: 13,
+                  fontFamily: 'Lexend Deca',
+                ),
+              )
+            ]
+          ),
         ],
       ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Stack(
-          children: [
-            Positioned(
-              left: 0,
-              top: 0,
-              right: 0,
-              bottom: 0,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 8, bottom: 24),
-                child: Text(
-                  text,
-                  style: TextStyle(
-                    color: Color(0xFF24252C),
-                    fontSize: 11,
-                    fontFamily: 'Lexend Deca',
-                    fontWeight: FontWeight.w400,
-                    height: 1.6,
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              right: 16,
-              bottom: 8,
-              child: Text(
-                _formatDate(registrationDate),
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 10,
-                  fontFamily: 'Lexend Deca',
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      // margin: EdgeInsets.symmetric(vertical: 8, horizontal: screenWidth * 0.05),
+      // height: screenHeight * 0.1,
+      // decoration: ShapeDecoration(
+      //   color: Colors.white,
+      //   shape: RoundedRectangleBorder(
+      //     borderRadius: BorderRadius.circular(15),
+      //   ),
+      //   shadows: [
+      //     BoxShadow(
+      //       color: Color(0x30000000),
+      //       blurRadius: 4,
+      //       offset: Offset(0, 5),
+      //       spreadRadius: 0,
+      //     ),
+      //   ],
+      // ),
+      // child: Padding(
+      //   padding: const EdgeInsets.symmetric(horizontal: 16),
+      //   child: Stack(
+      //     children: [
+      //       Positioned(
+      //         left: 0,
+      //         top: 0,
+      //         right: 0,
+      //         bottom: 0,
+      //         child: Padding(
+      //           padding: const EdgeInsets.only(top: 8, bottom: 24),
+      //           child: Text(
+      //             text,
+      //             style: TextStyle(
+      //               color: Color(0xFF24252C),
+      //               fontSize: 11,
+      //               fontFamily: 'Lexend Deca',
+      //               fontWeight: FontWeight.w400,
+      //               height: 1.6,
+      //             ),
+      //           ),
+      //         ),
+      //       ),
+      //       Positioned(
+      //         right: 16,
+      //         bottom: 8,
+      //         child: Text(
+      //           _formatDate(registrationDate),
+      //           style: TextStyle(
+      //             color: Colors.grey,
+      //             fontSize: 10,
+      //             fontFamily: 'Lexend Deca',
+      //             fontWeight: FontWeight.w400,
+      //           ),
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 
