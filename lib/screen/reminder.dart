4416ae1556b57b0babfc404251ccb58aac0fdb06
hjgel <202244111@itc.ac.kr>
2024-08-24 23:53:09 +0900

    2024.08.24 디자인 체크 후 변경

--- lib/screen/reminder.dart
+++ lib/screen/reminder.dart
@@ -55,15 +55,24 @@ class _ReminderState extends State<Reminder> with AutomaticKeepAliveClientMixin<
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     super.build(context);
     return Consumer<DisasterProvider>(
       builder: (context, provider, child) {
         return Scaffold(
           backgroundColor: Color(0xFFF0F1F0),
           appBar: AppBar(
-            title: Text('알림'),
+            title: Text(
+              '재난 상식',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: size.width * 0.06,
+                fontFamily: 'BM_HANNA_TTF',
+              ),
+            ),
             centerTitle: true,
-            backgroundColor: Colors.grey[200],
+            backgroundColor: const Color(0xEF537052),
             elevation: 0,
             actions: [
               IconButton(
