2208afd4e1bebab7eae0fce18818c8ae2e015074
hjgel <202244111@itc.ac.kr>
2024-08-28 20:08:26 +0900

    2024.08.28 로그인 페이지 고침

--- lib/screen/forgot.dart
+++ lib/screen/forgot.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pro_max_ject/main.dart';
-import 'signup.dart';
+import 'package:pro_max_ject/screen/login_screen.dart';
 
 class ForGot extends StatelessWidget {
   const ForGot({super.key});
@@ -9,47 +8,30 @@ class ForGot extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DefaultTabController(
-        length: 2, // Number of tabs
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              '회원 정보 찾기',
-              style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'BM_HANNA_TTF',
-              ),
-            ),
-            backgroundColor: const Color(0xEF537052),
-            centerTitle: true,
-            elevation: 4,
-            bottom: TabBar(
-              unselectedLabelColor: Colors.black,
-              labelColor: Colors.white,
-              indicator: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                // color: Colors.white,
-              ),
-              tabs: [
-                Tab(
-                  child: Text(
-                    '아이디 찾기',
-                  ),
-                ),
-                Tab(text: '비밀번호 찾기'),
-              ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            '회원 정보 찾기',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'BM_HANNA_TTF',
             ),
           ),
-          backgroundColor: const Color(0xFFF0F1F0),
-          body: TabBarView(
-            children: [
-              // 아이디 찾기 탭
-              _buildFindIdTab(),
-              // 비밀번호 찾기 탭
-              _buildFindPasswordTab(),
-            ],
+          backgroundColor: const Color(0xEF537052),
+          centerTitle: true,
+          elevation: 4,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginScreen()),
+              );
+            },
           ),
         ),
+        backgroundColor: const Color(0xFFF0F1F0),
+        body: _buildFindIdTab(), // 이 부분에서 단일 탭 화면을 사용합니다.
       ),
     );
   }
@@ -62,20 +44,6 @@ class ForGot extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const SizedBox(height: 16),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: '이름',
-                fillColor: Color(0xFFD9DED9),
-                filled: true,
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.person),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  borderSide: BorderSide(width: 0, color: Color(0x07658064)),
-                ),
-              ),
-            ),
             const SizedBox(height: 16),
             const TextField(
               decoration: InputDecoration(
@@ -101,69 +69,7 @@ class ForGot extends StatelessWidget {
               child: const Padding(
                 padding: EdgeInsets.symmetric(vertical: 16),
                 child: Text(
-                  '아이디 찾기',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  // Widget for 비밀번호 찾기 탭
-  Widget _buildFindPasswordTab() {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(height: 16),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: '이름',
-                fillColor: Color(0xFFD9DED9),
-                filled: true,
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.person),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  borderSide: BorderSide(width: 0, color: Color(0x07658064)),
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: '이메일',
-                fillColor: Color(0xFFD9DED9),
-                filled: true,
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.email),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  borderSide: BorderSide(width: 0, color: Color(0x07658064)),
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // 이메일 인증 전송 로직 구현
-              },
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(const Color(0xFF537052)),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 16),
-                child: Text(
-                  '이메일 인증 전송',
+                  '아이디 및 비빌번호 찾기',
                   style: TextStyle(
                     fontSize: 16,
                     fontWeight: FontWeight.bold,
